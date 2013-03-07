df3372afa2979657b548d56c62275e676a88b2ce
Zachary Quintana <zach.quintana@gmail.com>
2013-03-06 16:43:30 -0800

    Fix

--- autoload.php
+++ autoload.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function($class) {
-    $file = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+    $file = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
     if (is_file($file))
         require_once($file);
 });
